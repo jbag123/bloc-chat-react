5d73fced95e8fedd39ddebd8ae65fc37af858f60
Josh Cone <josh.cone@eastkent.ac.uk>
2018-06-18 20:36:55 +0100

    commiting list chat rooms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js';
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyAIEoSm2T85Fd85TY2K8bmHX3AjBkauwXQ",
@@ -15,15 +16,9 @@ firebase.initializeApp(config);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <RoomList
+        firebase={firebase}
+       />
     );
   }
 }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 
-class RoomList extends App {
+class RoomList extends Component {
   constructor(props) {
     super(props);
 
-
   this.state = {
     rooms: []
-  }
+  };
 
-  this.roomosRef = this.props.firebase.database().ref('rooms');
+  this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
   componentDidMount() {
-    this.roomosRef.on('child_added', snapshot => {
-      console.log(snapshot);
-    })
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key
+      this.setState({ rooms: this.state.rooms.concat( room ) });
+    });
   }
 
   render() {
     return(
-
-    )
+      <div>{this.state.rooms.map( (room) => <p>{room.name}</p> )}</div>
+    );
   }
 }
+
+export default RoomList;
